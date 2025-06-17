2cbed6ad0cacda6b2ad88880aa25358012955414
Bharath j r <bharathk9828@gmail.com>
2025-06-17 07:14:51 +0530

    Create Exp3.js

---
+++ Exp3.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import cs from '../Image/Img1.jpg';
+
+export default function Exp3() { 
+
+ return ( 
+
+ <div> 
+
+ <h1>Welcome to Rai Technology University</h1> 
+
+ <div> 
+
+ <img src={cs} /> 
+
+ </div> 
+
+ <h2>All rights are reserved at Rai Technology University</h2> 
+
+ <div> 
+
+ <p>Name: Bharath JR</p> 
+
+ <p>UEN: RTU24101CS020</p> 
+
+ </div> 
+
+ </div> 
+
+ );
+
+}
