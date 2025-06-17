c5dba4a2cf6b494e4bf1a394712d31012810d956
Bharath j r <bharathk9828@gmail.com>
2025-06-17 07:19:54 +0530

    Create Exp7.js

---
+++ Exp7.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+export default class Exp7 extends Component { 
+
+ render() { 
+
+ return ( 
+
+ <div style={{ textAlign: 'center', marginTop: '20px' }}> 
+
+ <p><strong>Name:</strong> D Suresh</p> 
+
+ <p><strong>Roll No.:</strong> RTU24101IT019</p> 
+
+ <div> 
+
+ <h1 className="header">Welcome to Rai Technology University</h1> 
+
+ <div className="form"> 
+
+ <form> 
+
+ <div className="form-group"> 
+
+ <label>Enter Username</label> 
+
+ <input type="text" placeholder="Enter username" /> 
+
+ </div> 
+
+ <div className="form-group"> 
+
+ <label>Enter Password</label> 
+
+ <input type="password" placeholder="Enter password" /> 
+
+ </div> 
+
+ <div className="button"> 
+
+ <input type="submit" value="Login" /> 
+
+ </div> 
+
+ </form> 
+
+ </div> 
+
+ </div>
+
+</div> 
+
+ );
+
+ } 
+
+}
